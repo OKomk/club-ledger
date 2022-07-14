9db42db06746f1d685ca63d5ea8f9e1d2dc5e01c
Pushkar <pushkar.umrao@gmail.com>
2022-07-14 23:25:53 +0530

    God Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+import secrets
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+app=Flask(__name__)
+
+SECRET_KEY = eval(getenv('SECRET_KEY'))
+print(SECRET_KEY)
+
+app.secret_key=SECRET_KEY # load some secret key from env
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method=='GET':
+        return render_template('home.html')
+    else :
+        return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
